3d736f26b4b14f629bf2aefb00c6ab11628aef32
Nicholas Ho <nicholas_hodj@outlook.com>
2014-09-19 15:24:23 +1000

    --registered.php-- Updated registered.php to be able to establish a connection with the GSBME server to gain access to the required database. Removed simple handling of cases where an applicant should be accepted or rejected. Replaced with more sophisticated conditional handling, using functions defined in functions.php. If Banned, give failure message. If not banned but already registered, give a different failure message. If not banned and not registered, give success message and write to DB.
    --functions.php--
    Containts 3 functions
    1) openConnection opens connection to server.
    2) isBanned checks if a particular applicant is flagged as banned on the DB.
    3) alreadyExists checks if a particular applicant has already applied and is not banned in the DB.
    
    --registrants.php--
    Serves a table showing the list of successfully registered applicants.

---
+++ functions.php
@@ -0,0 +1,56 @@
+<?php
+	// This function opens the connection to the database. Returns a reference to the open connection.
+	function openConnection() {
+		$conn = odbc_connect('z3422527', '', '',SQL_CUR_USE_ODBC);
+		return $conn;
+	}
+	
+	// This function opens a connection to the database and checks if the 
+	// name and email of a provided applicant matches any of the banned entries of the database.
+	// Return True if Yes.
+	function isBanned($given, $last, $email) {
+		$conn = openConnection();	
+		$regQueries = "SELECT * FROM Registration";
+		$applicants = odbc_exec($conn,$regQueries);
+
+		
+		while(odbc_fetch_row($applicants)) {
+			$dbGiven = strtolower(odbc_result($applicants,"GivenName"));
+			$dbLast = strtolower(odbc_result($applicants,"FamilyName"));
+			$dbEmail = strtolower(odbc_result($applicants,"Email"));
+			$banned = odbc_result($applicants,"Banned");
+			
+			if ((($dbGiven == strtolower($given) && 
+				$dbLast == strtolower($last)) || 
+				$dbEmail == strtolower($email)) &&
+				$banned) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// This function opens a connection to the database and checks if the 
+	// name and email of a provided applicant matches any of the unbanned entries of the database.
+	// Return True if Yes.
+	function alreadyExists($given, $last, $email) {
+		$conn = openConnection();	
+		$regQueries = "SELECT * FROM Registration";
+		$applicants = odbc_exec($conn,$regQueries);
+
+		while(odbc_fetch_row($applicants)) {
+			$dbGiven = strtolower(odbc_result($applicants,"GivenName"));
+			$dbLast = strtolower(odbc_result($applicants,"FamilyName"));
+			$dbEmail = strtolower(odbc_result($applicants,"Email"));
+			$banned = odbc_result($applicants,"Banned");
+			
+			if ((($dbGiven == strtolower($given) && 
+				$dbLast == strtolower($last)) || 
+				$dbEmail == strtolower($email)) &&
+				!$banned) {
+				return true;
+			}
+		}
+		return false;
+	}
+?>
--- registered.php
+++ registered.php
@@ -4,6 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Anti-zombie Symposium</title>
 	<link rel="stylesheet" type="text/css" href="style.css" />
+	<?php include("./functions.php"); ?>
 </head>
 
 <body>
@@ -21,7 +22,9 @@
 	</header>
 	<div class="intro-text">
 	    
-		<?php
+		<?php			
+			$conn = openConnection();	
+						
 			// Capture the contents of filled fields in the application form
 			$firstName = $_POST["firstName"];
        		$lastName = $_POST["lastName"];
@@ -29,26 +32,45 @@
        		$dateOfBirth = $_POST["dateOfBirth"];
        		// For Date of Birth, only need the Year of birth.
        		$yearOfBirth = substr($dateOfBirth, -4);
-       		// Use regexes to accout for Upper/lowercase first letter for the first and last names of Johnny Noshow
-       		$johnnyRegex = "/^[Jj]ohnny$/";
-       		$noShowRegex = "/^[Nn]oshow$/";
 
-       		// If Applicant's name is Johnny Noshow, do not let him register. 
-       		// Otherwise, allow registration and display entered details for confirmation.
-       		if (preg_match($johnnyRegex, $firstName) && preg_match($noShowRegex, $lastName)) {
-       			echo "<h1>Registration Unsuccessful - No-show policy</h1>
-					<h2>Details: </h2>";
-				echo "<p>Dear Mr. Noshow, last year you didn't turn up to present your talk. You cannot register this year!</p>";
-       		} else {
-       			echo "<h1>Registration Successful!</h1>
-					<p>You will hear from us shortly.</p>
-					<h2>Details: </h2>";
-       			echo "<strong>Name: </strong>" . $firstName . " " . $lastName . "<br>" .
-       				"<strong>Date of Birth: </strong>xx/xx/" . $yearOfBirth . "<br>" .
-       				"<strong>Email Address: </strong>" . $emailAddress . "<br>";
-       		}
+			echo "<br />";
+			
+			// Check the database if an entry for the applicant exists, and if he is banned. Display failure message if True.
+			if (isBanned($firstName, $lastName, $emailAddress)) {
+				echo "<h1>Registration Unsuccessful - No-show policy</h1>
+				<h2>Details: </h2>";
+				echo "<p>Dear Mr. ". $lastName .", last year you didn't turn up to present your talk. You cannot register this year!</p>";
+			// Check the database if an entry for the applicant exists, and he is not banned. Display failure message if True, as well as list of participants.
+			} elseif (alreadyExists($firstName, $lastName, $emailAddress)) {
+				echo "<h1>You have already registered!</h1>
+				<h2>Details: </h2>";
+				echo "<p>Dear Mr. ". $lastName .", you have already registered for the symposium! You may only register once.</p>";
+				include("./registrants.php");
+			// At this point, we can be sure that a DB entry does not exist for this participant. Create an entry in the DB, and display entered details, as well as list of participants.
+			} else {
+				$regDate = date("#Y-m-d#");
+				$sqlQuery = "INSERT INTO Registration (GivenName, FamilyName, Email, RegDate, Banned) VALUES ('$firstName', '$lastName', '$emailAddress', $regDate, False);";
+				$result = odbc_exec($conn,$sqlQuery);
+
+				echo "<h1>Registration Successful!</h1>
+				<p>You will hear from us shortly.</p>
+				<h2>Details: </h2>";
+				echo "<strong>Name: </strong>" . $firstName . " " . $lastName . "<br>" .
+				"<strong>Date of Birth: </strong>xx/xx/" . $yearOfBirth . "<br>" .
+				"<strong>Email Address: </strong>" . $emailAddress . "<br>";
+				include("./registrants.php");
+			}
+			
+			// Possible Alternate approach? 
+			/*
+			$bannedQuery = "SELECT * FROM Registration
+						WHERE Banned = True
+						AND (GivenName LIKE \"" . $firstName . "\"
+						AND FamilyName LIKE \"" . $lastName . "\"
+						OR EmailAddress LIKE \"" . $emailAddress . "\");
+						";
+			*/
 		?>
-
 	</div>
 </body>
 <hr>
@@ -56,3 +78,5 @@
 	<strong>Created by Nicholas Ho / z3422527</strong>
 </footer>
 </html>
+
+
